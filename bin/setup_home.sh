44d0db2c49eaf625a810d886ebf3d4bde79098ef
root <root@vhost1.localdomain>
2022-07-31 10:37:32 +0200

    setup_home.sh include create home

--- bin/setup_home.sh
+++ bin/setup_home.sh
@@ -9,6 +9,7 @@
 export LANG=C
 declare -r __SCRIPT_VERSION__='1.0'
 
+if ! grep -wq '\. ~/linux_home_root/bash_profile' ~/.bash_profile; then
 echo "Append to \"~/.bash_profile\":"
 echo "-------------------------------------------------------------------------------"
 tee -a $HOME/.bash_profile << END
@@ -18,3 +19,20 @@ if [[ -f ~/linux_home_root/bash_profile ]]; then
 fi
 END
 echo "-------------------------------------------------------------------------------"
+fi
+
+links=(
+  .gitconfig
+  .vimrc
+  .vim
+)
+
+for item in "${links[@]}"; do
+  if [[ -L /root/${item} ]]; then
+    ln -sf /root/linux_home_root/${item} /root/${item}
+  else
+    mv ${item} ${item}_$(date +%Y_%m_%d-%H_%M_%S)
+    ln -s /root/linux_home_root/${item} /root/{item}
+  fi
+done
+
